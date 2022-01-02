4efd9132b3384ffeb1f2e93f0b4881248d7d2fd9
Nive Parthiban <nive.parthiban@gmail.com>
2022-01-01 23:11:36 -0700

    Solved day 1

---
+++ day1/day1.py
@@ -0,0 +1,17 @@
+from time import perf_counter
+
+t1_start = perf_counter()
+
+depths = open("day1/day1input.txt").read().splitlines()
+
+numDepth = 0
+
+for i in range(0, len(depths) - 1):
+    if int(depths[i + 1]) > int(depths[i]):
+        numDepth = numDepth + 1
+
+print(numDepth)
+
+t1_stop = perf_counter()
+print("Elapsed time during the whole program in seconds:",
+      t1_stop - t1_start)
