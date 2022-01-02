d870b58c5b1bae6628b2f18f3f2b553ed0f84fbe
YoloWingPixie <83574853+YoloWingPixie@users.noreply.github.com>
2022-01-01 23:12:49 -0700

    My solutions are now better

--- the-better-solutions/day1-zmm.py
+++ the-better-solutions/day1-zmm.py
@@ -2,6 +2,7 @@ sonarData = []
 with open ('sonarreport.txt') as sonarIn:
     for line in sonarIn:
         sonarData.append(int(line))
+sonarIncrease = 0
 for i in range(len(sonarData)):
     if sonarData[i] > sonarData[i-1]:
         sonarIncrease += 1
