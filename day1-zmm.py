43c2c3a4885bdf094c3d1f7764c00b62a04aa25b
YoloWingPixie <83574853+YoloWingPixie@users.noreply.github.com>
2022-01-01 22:37:42 -0700

    Added sonarreport
    Added sonar report and Day1 solution.

---
+++ day1-zmm.py
@@ -0,0 +1,13 @@
+sonarData = []
+
+with open ('sonarreport.txt') as sonarIn:
+    for line in sonarIn:
+        sonarData.append(line)
+
+sonarIncrease = 0
+
+for i in range(len(sonarData)):
+    if sonarData[i] >= sonarData[i-1]:
+        sonarIncrease += 1
+
+print(sonarIncrease)
